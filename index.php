355f9826432fc5c0a3136bec93e332660197d2db
John Szetho <jszetho@MacBook-Pro.local>
2020-05-30 00:42:16 +1000

    look and feel

---
+++ index.php
@@ -0,0 +1,23 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP starter</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <script src="js/data.js"></script>
+</head>
+<body>
+    <main>
+        <h1>PHP starter</h1>
+        <p>First time testing <em>PHP</em> code.</p>
+        <?php
+            $fake_server = "<a href=\"\#\"\>192.28140.128";
+            echo "<p>You are currently running server: $fake_server</p>";
+        ?>
+    </main>
+    <footer>
+        <em><a href="https://johnszetho.com">johnszetho.com</a></em>
+    </footer>
+</body>
+</html>
